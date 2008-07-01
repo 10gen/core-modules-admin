8c3b105e1a4b0191d985bd930a9d1b4030a319c8
Ethan Glasser-Camp <ethan@10gen.com>
2008-07-01 12:36:55 -0400

    Check core-modules for an admin menu.

--- _init.js
+++ _init.js
@@ -9,7 +9,16 @@ admin.getAppNav = function(key){
     var appNav;
     if(core[key] && core[key].admin && core[key].admin.leftNav) appNav = core[key].admin.leftNav;
     else if(core.app[key] && core.app[key].admin && core.app[key].admin.leftNav) appNav = core.app[key].admin.leftNav;
-    else appNav = function(){ log.admin.debug("Can't get nav tree for application " + key); return{tree: [], reverse: []}; };
+    else {
+        try {
+            if(core.modules[key] && core.modules[key].admin && core.modules[key].admin.leftNav)
+                appNav = core.modules[key].admin.leftNav;
+        } catch(e) {
+            // couldn't access module and we got an exception
+
+        }
+    }
+    if (!appNav) appNav = function(){ log.admin.debug("Can't get nav tree for application " + key); return{tree: [], reverse: []}; };
     appNav = appNav();
     return appNav;
 };
